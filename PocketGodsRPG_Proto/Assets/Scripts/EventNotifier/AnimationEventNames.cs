3a164c7ba6b05f4f03ca634e631f358e7bac3f75
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-10-28 17:21:52 +0800

    Setting up scripts.

---
+++ PocketGodsRPG_Proto/Assets/Scripts/EventNotifier/AnimationEventNames.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Container for all animation event names that will play upon broadcast. We separate this from EventNames to avoid clutter.
+/// 
+/// Created By: NeilDG
+/// </summary>
+public class AnimationEventNames {
+
+	//wander king animation event
+	public const string START_KING_WANDER_ANIM = "START_KING_WANDER_ANIM";
+	public const string END_KING_WANDER_ANIM = "END_KING_WANDER_ANIM";
+}
