9d1fbe1876a125fa0e3c795d2de9263227d69d26
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-10-30 17:16:51 +0800

    Setting up game scene experiment.

---
+++ PocketGodsRPG_Proto/Assets/Game/Scripts/EventNotifier/EventNames.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Holder for event names
+ * Created By: NeilDG
+ */ 
+public class EventNames {
+	public const string ON_UPDATE_SCORE = "ON_UPDATE_SCORE";
+	public const string ON_DISPLAY_TEXT_UPDATE = "ON_DISPLAY_TEXT_UPDATE";
+	public const string ON_EMPTY_PARAMS_UPDATE = "ON_EMPTY_PARAMS_UPDATE";
+
+	public const string ON_MAP_PAN_DISABLED = "ON_MAP_PAN_DISABLED";
+	public const string ON_MAP_PAN_ENABLED = "ON_MAP_PAN_ENABLED";
+}
+
+
+
+
+
+
+
