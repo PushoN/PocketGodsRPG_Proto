18e3a447b87f3063b582612fff0cdc3202fd3e9f
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-11-06 17:39:22 +0800

    Battle system setup.

---
+++ PocketGodsRPG_Proto/Assets/Game/Scripts/BattleSystem/BattleSystemHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleSystemHandler : MonoBehaviour {
+
+	private static BattleSystemHandler sharedInstance = null;
+	public static BattleSystemHandler Instance {
+		get {
+			return sharedInstance;
+		}
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
