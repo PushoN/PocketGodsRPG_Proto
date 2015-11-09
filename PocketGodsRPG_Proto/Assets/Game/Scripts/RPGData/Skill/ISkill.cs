027738f994f335d532ab33a26ec125c722608e87
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-11-09 17:41:55 +0800

    Created representation for skills.

---
+++ PocketGodsRPG_Proto/Assets/Game/Scripts/RPGData/Skill/ISkill.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ISkill {
+
+	/// <summary>
+	/// Performs the skill on the specified controllable unit
+	/// </summary>
+	/// <param name="controllableUnit">Controllable unit.</param>
+	void Perform(ControllableUnit controllableUnit);
+}
---
+++ PocketGodsRPG_Proto/Assets/Game/Scripts/RPGData/Skill/NormalAttackSkill.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A normal attack skill representation
+/// </summary>
+public class NormalAttackSkill : ISkill {
+
+	public void Perform(ControllableUnit controllableUnit) {
+		Debug.Log ("Normal attack skill to " +controllableUnit);
+	}
+}
