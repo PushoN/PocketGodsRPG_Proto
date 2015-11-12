876b704e03ae2499773a1ffb4fe90a0b9e384cc1
Neil Del Gallego <neil.dg@pocketplaylab.com>
2015-11-12 17:12:42 +0800

    Profile manager

---
+++ PocketGodsRPG_Proto/Assets/Game/Scripts/UserProfile/ProfileManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents the manager for list of profiles available
+/// </summary>
+public class ProfileManager : MonoBehaviour {
+
+	private static ProfileManager sharedInstance = null;
+	public ProfileManager Instance {
+		get {
+			if(sharedInstance == null) {
+				sharedInstance = new ProfileManager();
+			}
+		}
+	}
+
+	private ProfileManager() {
+
+	}
+}
